038b8a10b58e1a228fe9568e5cd7954a1ce7c5fc
USER <USER@DESKTOP-SDPQRM5>
2022-06-21 00:32:41 +0900

    First commit in dev branch

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,6 +3,8 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("Hello Git!");
 		System.out.println("This is ??? version");
+		MyClass myclass = new MyClass();
+		myclass.method();
 	}
 
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -1,7 +1,15 @@
+class MyClass{
+    public void method(){
+        System.out.println("ABC");
+    }
+}
+
 
 public class B {
 	public static void main(String args[]) {
-		System.out.println();
+	 MyClass myclass = new MyClass();
+	 myclass.method();
 	}
-
+	
 }
+
